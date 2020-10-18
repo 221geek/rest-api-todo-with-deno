a526eda1fc33d771410806a7a0f969886d5b4741
221geek <abdoulazizdione1997@gmail.com>
2020-10-18 15:41:42 +0000

    commit

--- todo.ts
+++ todo.ts
@@ -15,6 +15,21 @@ export class TodoList extends Drash.Http.Resource {
     this.response.body = todos;
     return this.response;
   }
+
+  public POST() {
+    const id = Math.floor(Math.random() * 100);
+    while (todos.find((task) => task.id == id)) {
+      const id = Math.floor(Math.random() * 100);
+    }
+    const task: Task = {
+      id: id,
+      title: String(this.request.getBodyParam("title")),
+      completed: Boolean(this.request.getBodyParam("completed")),
+    };
+    todos.push(task);
+    this.response.body = task;
+    return this.response;
+  }
 }
 
 export class TodoElement extends Drash.Http.Resource {
@@ -30,6 +45,38 @@ export class TodoElement extends Drash.Http.Resource {
         `Task with id ${URL_param} not found`,
       );
     }
+    this.response.body = task;
+
+    return this.response;
+  }
+
+  public DELETE() {
+    const URL_param = this.request.getPathParam("id");
+    const task = todos.find((task) => task.id === Number(URL_param));
+
+    if (!task) {
+      throw new Drash.Exceptions.HttpException(
+        404,
+        `Task with id ${URL_param} not found`,
+      );
+    }
+    todos = todos.filter((task) => task.id != Number(URL_param));
+    this.response.body = "task deleted";
+
+    return this.response;
+  }
+
+  public PUT() {
+    const URL_param = this.request.getPathParam("id");
+    const task = todos.find((task) => task.id === Number(URL_param));
+
+    if (!task) {
+      throw new Drash.Exceptions.HttpException(
+        404,
+        `Task with id ${URL_param} not found`,
+      );
+    }
+    task.completed = Boolean(this.request.getBodyParam("completed"));
     this.response.body = task;
 
     return this.response;
